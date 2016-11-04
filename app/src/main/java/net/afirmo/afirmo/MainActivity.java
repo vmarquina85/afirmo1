0a959e141567551233e2079bb52d885a69b3f522
Victor A. Marquina Cardenas <victor.marquina@SISOL.GOB.PE>
2016-11-03 19:30:58 -0500

    Se Agrego Funcionabilidad de Camara Android

--- app/src/main/java/net/afirmo/afirmo/MainActivity.java
+++ app/src/main/java/net/afirmo/afirmo/MainActivity.java
@@ -1,17 +1,57 @@
 package net.afirmo.afirmo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends ActionBarActivity {
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.buttonCamara);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file=getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT,Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
+            }
+        });
+
+
+    }
+
+    private File getFile() {
+        File folder = new File("sdcard/afirmo_app");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        File image_file = new File(folder, "cam_image.jpg");
+        return image_file;
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+       String path="sdcard/afirmo_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
     }
 }
